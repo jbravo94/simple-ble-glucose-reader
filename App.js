1288e2e241c45ff7acaa75e3bb6a226151185da1
Johannes HEINZL <johannes.heinzl@vamed.com>
2022-06-11 19:57:07 +0200

    Refactoring

--- App.js
+++ App.js
@@ -22,41 +22,15 @@ import {
   TouchableHighlight,
   Alert,
 } from 'react-native';
-import {stringToBytes, bytesToString} from 'convert-string';
+import {bytesToString} from 'convert-string';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 import BleManager from 'react-native-ble-manager/BleManager';
 const BleManagerModule = NativeModules.BleManager;
 const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 import BluetoothStateManager from 'react-native-bluetooth-state-manager';
-import RNBluetoothClassic, {
-  BluetoothEventType,
-} from 'react-native-bluetooth-classic';
-
-// https://github.com/NightscoutFoundation/xDrip/blob/c48ef307c6dd28f6f6027a775a2b82c89a5b281d/app/src/main/java/com/eveningoutpost/dexdrip/glucosemeter/BluetoothCHelper.java#L9
-const getSfloat16 = (b0, b1) => {
-  const mantissa = unsignedToSigned(
-    unsignedByteToInt(b0) + ((unsignedByteToInt(b1) & 0x0f) << 8),
-    12,
-  );
-  const exponent = unsignedToSigned(unsignedByteToInt(b1) >> 4, 4);
-  return mantissa * Math.pow(10, exponent);
-};
-
-const unsignedByteToInt = b => {
-  return b & 0xff;
-};
-
-const unsignedBytesToInt = (b, c) => {
-  return ((unsignedByteToInt(c) << 8) + unsignedByteToInt(b)) & 0xffff;
-};
-
-const unsignedToSigned = (unsigned, size) => {
-  if ((unsigned & (1 << (size - 1))) != 0) {
-    unsigned = -1 * ((1 << (size - 1)) - (unsigned & ((1 << (size - 1)) - 1)));
-  }
-  return unsigned;
-};
+import RNBluetoothClassic from 'react-native-bluetooth-classic';
+import GlucoseReadingRx from './GlucoseReadingRx';
 
 const App = () => {
   // Service
@@ -67,6 +41,9 @@ const App = () => {
   const GLUCOSE_CHARACTERISTIC = '00002a18-0000-1000-8000-00805f9b34fb';
   const CONTEXT_CHARACTERISTIC = '00002a34-0000-1000-8000-00805f9b34fb';
   const RECORDS_CHARACTERISTIC = '00002a52-0000-1000-8000-00805f9b34fb';
+
+  let receiveTimeout = null;
+  let results = '';
 
   const [isScanning, setIsScanning] = useState(false);
   const [peripherals, setPeripherals] = useState(new Map());
@@ -79,7 +56,6 @@ const App = () => {
     if (!isScanning) {
       BleManager.scan([], 3, true)
         .then(results => {
-          console.log('Scanning...');
           setIsScanning(true);
         })
         .catch(err => {
@@ -89,125 +65,43 @@ const App = () => {
   };
 
   const handleStopScan = () => {
-    console.log('Scan is stopped');
     setIsScanning(false);
+  };
+
+  const setConnectedPeripheral = peripheralId => {
+    let p = peripherals.get(peripheralId);
+    if (p) {
+      setConnectedPeripheralId(peripheralId);
+      p.connected = true;
+      peripherals.set(peripheralId, p);
+      setList(Array.from(peripherals.values()));
+    }
+  };
+
+  const unsetConnectedPeripheral = peripheralId => {
+    let p = peripherals.get(peripheralId);
+    if (p) {
+      setConnectedPeripheralId('');
+      p.connected = false;
+      peripherals.set(peripheralId, p);
+      setList(Array.from(peripherals.values()));
+    }
   };
 
   const handleDisconnectedPeripheral = data => {
     if (connectedPeripheralId === data.peripheral) {
-      setConnectedPeripheralId('');
+      unsetConnectedPeripheral(data.peripheral);
     }
-
-    console.log('Disconnected from ' + data.peripheral);
   };
 
-  let receiveTimeout = null;
-  let results = '';
-
   const handleUpdateValueForCharacteristic = data => {
-    console.log(
-      'Received data from ' +
-        data.peripheral +
-        ' characteristic ' +
-        data.characteristic,
-      data.value,
-    );
-
     if (data.characteristic === GLUCOSE_CHARACTERISTIC) {
-      const bytes = data.value;
+      const result = new GlucoseReadingRx(data.value);
 
-      const flags = bytes[0];
+      results = results + result.toString() + '\n';
 
-      const timeOffsetPresent = (flags & 0x01) > 0;
-      const typeAndLocationPresent = (flags & 0x02) > 0;
-      const concentrationUnitKgL = (flags & 0x04) == 0;
-      const sensorStatusAnnunciationPresent = (flags & 0x08) > 0;
-      const contextInfoFollows = (flags & 0x10) > 0;
-
-      // Little-endian ordering
-      const sequence = (bytes[2] << 8) + bytes[1];
-
-      // Little-endian ordering
-      const year = (bytes[4] << 8) + bytes[3];
-      const month = bytes[5];
-      const day = bytes[6];
-      const hour = bytes[7];
-      const minute = bytes[8];
-      const second = bytes[9];
-
-      const date = new Date(year, month - 1, day, hour, minute, second);
-
-      let offset;
-      let kgl;
-      let mgdl;
-
-      let ptr = 10;
-      if (timeOffsetPresent) {
-        offset = bytes[ptr] + bytes[ptr + 1] * 256;
-        ptr += 2;
-      }
-
-      if (concentrationUnitKgL) {
-        kgl = getSfloat16(bytes[ptr], bytes[ptr + 1]);
-        mgdl = kgl * 100000;
-      } else {
-        Alert.alert('mmol not implemented!');
-      }
-      ptr += 2;
-
-      let typeAndLocation;
-      let sampleLocation;
-      let sampleType;
-
-      if (typeAndLocationPresent) {
-        typeAndLocation = bytes[ptr];
-        sampleLocation = (typeAndLocation & 0xf0) >> 4;
-        sampleType = typeAndLocation & 0x0f;
-        ptr = ptr + 1;
-      }
-
-      let status;
-
-      if (sensorStatusAnnunciationPresent) {
-        status = bytes[ptr];
-      }
-
-      console.log(
-        'Glucose data: mg/dl: ' +
-          (mgdl > 10000 ? '-INFINITY' : mgdl) +
-          '  kg/l: ' +
-          (kgl > 1 ? '-INFINITY' : kgl) +
-          '  seq:' +
-          sequence +
-          ' sampleType: ' +
-          sampleType +
-          '  sampleLocation: ' +
-          sampleLocation +
-          '  time: ' +
-          hour +
-          ':' +
-          minute +
-          ':' +
-          second +
-          '  ' +
-          day +
-          '-' +
-          month +
-          '-' +
-          year +
-          ' timeoffset: ' +
-          offset,
-      );
-
-      results =
-        results +
-        date.toString() +
-        ' - ' +
-        (mgdl < 0 ? '-INFINITY' : mgdl) +
-        ' mg/dl' +
-        '\n';
       if (receiveTimeout) {
-        clearTimeout(receiveTimeout); //cancel the previous timer.
+        clearTimeout(receiveTimeout);
         receiveTimeout = null;
       }
       receiveTimeout = setTimeout(showResults, 500);
@@ -215,7 +109,7 @@ const App = () => {
   };
 
   const showResults = () => {
-    Alert.alert('Glucose data:', results);
+    Alert.alert('Glucose data', results);
     results = '';
   };
 
@@ -228,10 +122,7 @@ const App = () => {
       for (var i = 0; i < results.length; i++) {
         const peripheral = results[i];
         disconnect(peripheral.id).then(() => {
-          setConnectedPeripheralId('');
-          peripheral.connected = false;
-          peripherals.set(peripheral.id, peripheral);
-          setList(Array.from(peripherals.values()));
+          unsetConnectedPeripheral(peripheral.id);
         });
       }
     });
@@ -264,7 +155,6 @@ const App = () => {
     BleManager.read(deviceId, DEVICE_INFO_SERVICE, MANUFACTURER_NAME)
       .then(data => {
         const manufacturerName = bytesToString(data);
-        console.log(manufacturerName);
 
         switch (manufacturerName) {
           case 'Roche':
@@ -318,13 +208,7 @@ const App = () => {
 
   const disconnect = peripheralId => {
     return BleManager.disconnect(peripheralId).then(() => {
-      let p = peripherals.get(peripheralId);
-      if (p) {
-        setConnectedPeripheralId('');
-        p.connected = false;
-        peripherals.set(peripheralId, p);
-        setList(Array.from(peripherals.values()));
-      }
+      unsetConnectedPeripheral(peripheralId);
     });
   };
 
@@ -353,17 +237,8 @@ const App = () => {
       } else {
         BleManager.connect(peripheral.id)
           .then(() => {
-            let p = peripherals.get(peripheral.id);
-
-            if (p) {
-              p.connected = true;
-              peripherals.set(peripheral.id, p);
-              setList(Array.from(peripherals.values()));
-
-              setConnectedPeripheralId(peripheral.id);
-              getManufacturerName(peripheral.id);
-            }
-            console.log('Connected to ' + peripheral.id);
+            setConnectedPeripheral(peripheral.id);
+            getManufacturerName(peripheral.id);
           })
           .catch(error => {
             console.log('Connection error', error);
